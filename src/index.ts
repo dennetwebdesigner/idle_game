29dfd07202f562e539f9e82298a72555926e9858
dennetwebdesigner <dennetwebdesigner@gmail.com>
2025-07-21 15:39:36 -0300

    first

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors({ origin: '*' }));
+
+const wss = new WebSocket.Server({ noServer: true });
+
+// Lida com conexões WebSocket
+wss.on('connection', (ws) => {
+  console.log('📡 Cliente WebSocket conectado');
+
+  ws.on('message', (message) => {
+    console.log('📩 Mensagem WS:', message.toString());
+
+    // Echo simples
+    // ws.send(`Recebido: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('🔌 Cliente WS desconectado');
+  });
+});
+
+// Faz o upgrade manual e aceita qualquer origem
+server.on('upgrade', (req, socket, head) => {
+  console.log('🔄 Upgrade solicitado');
+
+  // Aqui você pode checar o origin se quiser
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    wss.emit('connection', ws, req); // Emite uma nova conexão com o cliente
+  });
+});
+
+const PORT = process.env.PORT || 3333;
+
+server.listen(PORT, () => console.log(`Server online in port ${PORT}`));
